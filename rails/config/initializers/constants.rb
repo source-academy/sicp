710d25abe0a58190833838ebb528e1782ccf5389
Joy <piaozai88@163.com>
2018-06-17 11:43:21 +0800

    clean some code

--- rails/config/initializers/constants.rb
+++
@@ -1,7 +0,0 @@
-# Default values
-# Change IDE_PREFIX and GCSE_CX here before deploying
-# Set up Google Custom Search here: https://cse.google.com/cse/create/new
-
-IDE_PREFIX="http://www.comp.nus.edu.sg/~cs1101s/sicp/"
-GCSE_CX="014276565958104831575:mkaqp5nceyk"
-LANGUAGE_VERSION="javascript"
