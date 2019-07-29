b41696379a9ee12e6c15c021285ac14a4fbe0841
Tobias Wrigstad <tobias.wrigstad@it.uu.se>
2019-07-30 00:57:08 +0800

    Updating web SICP to look like the PDF

--- rails/config/application.rb
+++ rails/config/application.rb
@@ -11,5 +11,8 @@ module SicpTextbookRails
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.assets.enabled = true  
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
   end
 end
+
