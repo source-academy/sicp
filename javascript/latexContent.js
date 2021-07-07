781f3457832a26a70eca330b0d2cbde9d0261213
Martin Henz <henz@comp.nus.edu.sg>
2021-07-07 10:47:13 +0800

    section hyphenation

--- javascript/latexContent.js
+++ javascript/latexContent.js
@@ -42,7 +42,7 @@ export const preamble = `\\documentclass[7x10]{../mitpress/mit}
                 \\item[]\\hspace*{-1ex}}
                {\\endlist}
 
-\\hyphenation{ECMA-Script where-by produc-ing con-clu-sion in-for-ma-tion}
+\\hyphenation{ECMA-Script where-by produc-ing con-clu-sion in-for-ma-tion sec-tion}
 
 \\usepackage{etoolbox}
 \\makeatletter
