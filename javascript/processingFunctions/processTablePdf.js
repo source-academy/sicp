4ca50185fc5981fd5d27183dbe8a53806f2c96fe
Martin Henz <henz@comp.nus.edu.sg>
2021-08-01 15:51:11 +0800

    julie 1/8

--- javascript/processingFunctions/processTablePdf.js
+++ javascript/processingFunctions/processTablePdf.js
@@ -4,7 +4,9 @@ export const processTable = (node, writeTo) => {
   const firstRow = node.getElementsByTagName("TR")[0];
   if (firstRow) {
     const colNum = firstRow.getElementsByTagName("TD").length;
-    writeTo.push("\n\\begin{quote}\n\\noindent\n\\bgroup\\def\\arraystretch{1.5}\\begin{tabular}{");
+    writeTo.push(
+      "\n\\begin{quote}\n\\noindent\n\\bgroup\\def\\arraystretch{1.5}\\begin{tabular}{"
+    );
     for (let i = 0; i < colNum; i++) {
       writeTo.push("l  ");
     }
@@ -21,18 +23,17 @@ export const processTable = (node, writeTo) => {
         }
         recursiveProcessTextLatex(col.firstChild, writeTo);
       }
-	let nextRow = row.nextSibling;
-	while (nextRow && nextRow.nodeName != "TR") {
-	    nextRow = nextRow.nextSibling;
-	}
-	if (nextRow && nextRow.getAttribute("SINGLESPACE") === "yes") {
-	    writeTo.push(" \\\\[-6pt]\n");
-	} else if
-	    (nextRow && nextRow.getAttribute("DOUBLESPACE") === "yes") {
-	    writeTo.push(" \\\\[6pt]\n");
-	} else {
-	    writeTo.push(" \\\\ \n");
-	}
+      let nextRow = row.nextSibling;
+      while (nextRow && nextRow.nodeName != "TR") {
+        nextRow = nextRow.nextSibling;
+      }
+      if (nextRow && nextRow.getAttribute("SINGLESPACE") === "yes") {
+        writeTo.push(" \\\\[-6pt]\n");
+      } else if (nextRow && nextRow.getAttribute("DOUBLESPACE") === "yes") {
+        writeTo.push(" \\\\[6pt]\n");
+      } else {
+        writeTo.push(" \\\\ \n");
+      }
     }
     writeTo.push("\\end{tabular}\\egroup\n\\end{quote}\n");
   } else {
