2507600bf70e871fb792f5d35620537fe5e47e5e
Tobias Wrigstad <tobias.wrigstad@it.uu.se>
2021-08-28 19:34:59 +0200

    courtesy MIT Museum

--- javascript/latexContent.js
+++ javascript/latexContent.js
@@ -815,7 +815,7 @@ visit \\url{https://www.gnu.org/licenses/gpl-3.0.html}.
 \\vspace{3mm}
 
 The original image of MIT founder William Barton Rogers
-in section 2.2.4 is courtesy the MIT Museum.
+in section 2.2.4 is courtesy MIT Museum.
 
 The cover image is adapted from \\emph{Le Moyen Age et la Renaissance},  
 Paris, 1848--1851.
