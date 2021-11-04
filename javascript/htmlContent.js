38ede640adcebaa32829ab757840ba6e0e975da4
Martin Henz <henz@comp.nus.edu.sg>
2021-11-04 15:18:57 +0800

    license clarification of comparison edition

--- javascript/htmlContent.js
+++ javascript/htmlContent.js
@@ -1,5 +1,5 @@
 const shortTitleDefault = `SICP &mdash; JS`;
-const longTitleDefault = `Structure and Interpretation of Computer Programs &mdash; JavaScript Edition`;
+const longTitleDefault = `Structure and Interpretation of Computer Programs &mdash; Comparison Edition`;
 let shortTitle = shortTitleDefault;
 let longTitle = longTitleDefault;
 let this_edition = `
@@ -40,7 +40,7 @@ export const switchTitle = version => {
     longTitle = `Structure and Interpretation of Computer Programs &mdash; Comparison Edition`;
     this_edition = `
     <div class="title-text-EDITION">
-       <span class="title-text-EDITION">Scheme-JavaScript Comparison Edition</span>
+       <span class="title-text-EDITION">Comparison Edition</span>
     </div>`;
     legend = `
     <div class="title-text-ALSO">
@@ -53,7 +53,7 @@ export const switchTitle = version => {
     </div>
     <div class="title-text-OTHEREDITIONS">
       <span class="title-text-OTHEREDITIONS">
-      <span style="color:green">Original █</span>
+      <span style="color:green">Scheme █</span>
       </span>
     </div>
     <div class="title-text-OTHEREDITIONS">
@@ -183,7 +183,7 @@ export const html_links_part2 = (writeTo, toIndexFolder, version) => {
     </div>
     <div class="title-text-OTHEREDITIONS">
       <span class="title-text-OTHEREDITIONS">
-      <span style="color:green">Original █</span>
+      <span style="color:green">Scheme █</span>
       </span>
     </div>
     <div class="title-text-OTHEREDITIONS">
@@ -254,23 +254,16 @@ export const beforeContentWrapper = `<div id='permalink-msg'>
 `;
 
 export const html_licences = `<div class="title-text-LICENCE">
-<a href="http://creativecommons.org/licenses/by-nc-sa/4.0/" rel="nofollow"><img src="https://licensebuttons.net/l/by-nc-sa/4.0/88x31.png" style="max-width:100%;"></a>
+<a href="http://creativecommons.org/licenses/by-sa/4.0/" rel="nofollow"><img src="https://licensebuttons.net/l/by-sa/4.0/88x31.png" style="max-width:100%;"></a>
 <p/>
-This work is licensed under a <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/" rel="nofollow">Creative Commons Attribution-NonCommercial-ShareAlike 4.0
-International License</a>.
+The text of the original SICP 2nd edition is licensed by Harold Abelson and Gerald Jay Sussman under a <a href="http://creativecommons.org/licenses/by-sa/4.0/" rel="nofollow">Creative Commons Attribution-ShareAlike 4.0
+International License</a> (CC BY-SA). The text of JavaScript adaptation is licensed by Harold Abelson, Gerald Jay Sussman, Martin Henz, and Tobias Wrigstad, also under CC BY-SA. The figures in the JavaScript adaptation are derived from figures created by Andres Raba in 2015 and are licensed by Martin Henz and Tobias Wrigstad, also under CC BY-SA.
      </div>
 
      <div class="title-text-LICENCE">
 <a href="https://www.gnu.org/licenses/gpl-3.0.en.html" rel="nofollow"><img src="https://camo.githubusercontent.com/46d38fe6087a9b9bdf7e45458901b818765b8391/68747470733a2f2f75706c6f61642e77696b696d656469612e6f72672f77696b6970656469612f636f6d6d6f6e732f7468756d622f372f37392f4c6963656e73655f69636f6e2d67706c2e7376672f353070782d4c6963656e73655f69636f6e2d67706c2e7376672e706e67" alt="GPL 3" data-canonical-src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/79/License_icon-gpl.svg/50px-License_icon-gpl.svg.png" style="max-width:100%;"></a>
 <p/>
-All JavaScript programs in this work are licensed under the
-<a href="https://www.gnu.org/licenses/gpl-3.0.en.html" rel="nofollow">GNU General Public License Version 3</a>.
-     </div>
-
-    <div class="title-text-LICENCE">
-<a href="http://creativecommons.org/licenses/by-nc-sa/4.0/" rel="nofollow"><img src="https://licensebuttons.net/l/by-nc-sa/4.0/88x31.png" style="max-width:100%;"></a>
-<p/>
-The final version of this work will be published by The MIT Press under a <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/" rel="nofollow">Creative Commons Attribution-NonCommercial-ShareAlike 4.0
-International License</a>.
+All Scheme programs in this work are licensed by Harold Abelson and Gerald Jay Sussman under the
+<a href="https://www.gnu.org/licenses/gpl-3.0.en.html" rel="nofollow">GNU General Public License Version 3</a> (GPLv3). All JavaScript programs in this work are licensed by Martin Henz and Tobias Wrigstad, also under GPLv3.
      </div>
 `;
