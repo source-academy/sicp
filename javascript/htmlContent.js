23b568db05652367ebc5e9bdc20922a0795989a9
Martin Henz <henz@comp.nus.edu.sg>
2021-11-04 15:45:04 +0800

    minor

--- javascript/htmlContent.js
+++ javascript/htmlContent.js
@@ -257,7 +257,7 @@ export const html_licences = `<div class="title-text-LICENCE">
 <a href="http://creativecommons.org/licenses/by-sa/4.0/" rel="nofollow"><img src="https://licensebuttons.net/l/by-sa/4.0/88x31.png" style="max-width:100%;"></a>
 <p/>
 The text of the original SICP 2nd edition is licensed by Harold Abelson and Gerald Jay Sussman under a <a href="http://creativecommons.org/licenses/by-sa/4.0/" rel="nofollow">Creative Commons Attribution-ShareAlike 4.0
-International License</a> (CC BY-SA). The text of JavaScript adaptation is licensed by Harold Abelson, Gerald Jay Sussman, Martin Henz, and Tobias Wrigstad, also under CC BY-SA. The figures in the JavaScript adaptation are derived from figures created by Andres Raba in 2015 and are licensed by Martin Henz and Tobias Wrigstad, also under CC BY-SA.
+International License</a> (CC BY-SA). The text of the JavaScript adaptation is licensed by Harold Abelson, Gerald Jay Sussman, Martin Henz, and Tobias Wrigstad, also under CC BY-SA. The figures in the JavaScript adaptation are derived from figures created by Andres Raba in 2015 and are licensed by Martin Henz and Tobias Wrigstad, also under CC BY-SA.
      </div>
 
      <div class="title-text-LICENCE">
