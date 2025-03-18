d443ffb889c970a38c753571726f485a44c10ff1
yihao03 <yihao03@gmail.com>
2025-03-18 23:36:19 +0800

    Refactor async execution in fancyName function and improve error handling in Promise.all

--- i18n/index.ts
+++ i18n/index.ts
@@ -3,27 +3,26 @@ import PathGenerator from "./controllers/path.ts";
 import translate from "./controllers/recurTranslate.ts";
 
 export default async function fancyName(path: string) {
-  const startTime = new Date().getTime();
 
   const fullPath = PathGenerator(path);
-  console.log("Translating: " + fullPath);
   await translate("Chinese", fullPath);
-
-  const elapsed = new Date().getTime() - startTime;
-  console.log(fullPath + " took " + elapsed / 1000.0 + " seconds");
 }
 
-
-
-await Promise.all([
-  fancyName("2")
-  // fancyName("1.1"),
-  // fancyName("1.1.2"),
-  // fancyName("1.1.3"),
-  // fancyName("1.1.4"),
-  // fancyName("1.1.5"),
-  // fancyName("1.1.6"),
-  // fancyName("1.1.7"),
-  // fancyName("1.1.8"),
-  // translate("Chinese", "1"),
-]);
+(async () => {
+  try {
+    await Promise.all([
+      // fancyName("2")
+      // fancyName("1.1"),
+      // fancyName("1.1.2"),
+      // fancyName("1.1.3"),
+      // fancyName("1.1.4"),
+      // fancyName("1.1.5"),
+      fancyName("1.1.6"),
+      // fancyName("1.1.7")
+      // fancyName("1.1.8"),
+      // translate("Chinese", "1"),
+    ]);
+  } catch (e) {
+    console.error(e);
+  }
+})();
