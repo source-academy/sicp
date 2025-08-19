9c19481d1fa69559ff486c9e0e82c89f7a522d4f
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2025-08-20 01:35:31 +0800

    Migrate constants to TS

--- javascript/constants.ts
+++ javascript/constants.ts
@@ -1,8 +1,5 @@
 export const sourceAcademyURL = "https://sourceacademy.org";
 
-// to change to localhost if required
-// http://localhost:8075
-
 export const authors = "Harold Abelson and Gerald Jay Sussman";
 
 export const authors_with = "Julie Sussman";
