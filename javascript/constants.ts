4ab33009d0d01832c9f4167cd6d05e7b970dacfb
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2025-08-20 04:03:47 +0800

    Reformat

--- javascript/constants.ts
+++ javascript/constants.ts
@@ -1,9 +1,5 @@
 export const sourceAcademyURL = "https://sourceacademy.org";
-
 export const authors = "Harold Abelson and Gerald Jay Sussman";
-
 export const authors_with = "Julie Sussman";
-
 export const adapters = "Martin Henz and Tobias Wrigstad";
-
 export const adapters_with = "Samuel Fang";
